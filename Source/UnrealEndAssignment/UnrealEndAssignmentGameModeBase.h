d610dad5c8a934f1c9b3f8e7ac324da17326b76e
arne six <six.arne@gmail.com>
2022-11-21 10:27:12 +0100

    first commit

---
+++ Source/UnrealEndAssignment/UnrealEndAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealEndAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALENDASSIGNMENT_API AUnrealEndAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
